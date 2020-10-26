3db857e9f304af3d32e979d64e165fedcc23fb4e
admiral <amamadf@gmail.com>
2020-10-26 20:49:02 +0330

    show Raw.load

--- packet_sniffer.py
+++ packet_sniffer.py
@@ -1,9 +1,11 @@
 import scapy.all as scapy
-
+from scapy.layers import http
 def sniff(interface):
     scapy.sniff(iface="wlp58s0",store=False,prn=sniffed_packet)
 
 def sniffed_packet(packet):
-    print(packet)
+    if packet.haslayer(http.HTTPRequest):
+         if packet.haslayer(scapy.Raw):
+            print(packet[scapy.Raw].load)
 
 sniff("wlp58s0")

